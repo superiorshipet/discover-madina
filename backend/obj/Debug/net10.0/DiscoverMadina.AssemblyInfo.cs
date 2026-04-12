377a9bf74e4d2fee0edbaa770f9cf4af7c36471b
superior <superiorshipet@gmail.com>
2026-04-12 05:43:01 +0200

    the data

--- backend/obj/Debug/net10.0/DiscoverMadina.AssemblyInfo.cs
+++ backend/obj/Debug/net10.0/DiscoverMadina.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiscoverMadina")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f01a0ccfced9cdfff3d1ee4049e23f53a5e1d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8c453890c31bcbddff248d542a2a981a6d0077f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiscoverMadina")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiscoverMadina")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
